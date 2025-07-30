6527cd1baffeb98653873f922eeb822c6b762555
Hassan Allam <hassan309pro@gmail.com>
2025-07-31 01:28:56 +0300

    Q1 :  Given an array  consists of  numbers with size N and number of queries, in each query you will be given an integer X, and you should print how many numbers in array that is greater than  X.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Advanced_C__02
+﻿using System.Collections;
+
+namespace Advanced_C__02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Console.WriteLine("Enter N And Q");
+            //string[] Input = Console.ReadLine().Split();
+            //int N = Convert.ToInt32(Input[0]);
+            //int Q = Convert.ToInt32(Input[1]);
+            //
+            //ArrayList arrayList = new ArrayList();
+            //string[] elements = Console.ReadLine().Split();
+            //foreach (string s in elements)
+            //{
+            //    arrayList.Add(int.Parse(s));
+            //}
+            //
+            //for (int i = 0; i < Q; i++)
+            //{
+            //    int X = Convert.ToInt32(Console.ReadLine());
+            //    int Count = 0;
+            //
+            //    foreach (int Num in arrayList)
+            //    {
+            //        if (Num > X)
+            //        {
+            //            Count++;
+            //        }
+            //        
+            //    }
+            //    Console.WriteLine(Count);
+            //}
+            #endregion
         }
     }
 }
