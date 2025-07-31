e7b42636a9e09b592466082e2c7ae4c49da1339f
Hassan Allam <hassan309pro@gmail.com>
2025-08-01 00:47:59 +0300

    Q5 :  Given an array, implement a function to remove duplicate elements from an array.

--- Program.cs
+++ Program.cs
@@ -82,54 +82,83 @@ namespace Advanced_C__02
             #endregion
 
             #region Q4
-           // string input = "[()]{}";
-           // bool isBalanced = IsBalance(input);
-           // Console.WriteLine(isBalanced ? "Balanced" : "Not Balanced");
+            //string input = "[()]{}";
+            //bool isBalanced = IsBalance(input);
+            //Console.WriteLine(isBalanced ? "Balanced" : "Not Balanced");
             #endregion
 
-
+            #region Q5
+            //int[] array = { 10, 15, 7, 10, 12, 7, 30, 31, 30, 15 };
+            //int[] uniqueArray = RemoveDuplicates(array);
+            //
+            //Console.WriteLine("The Arrar After Remove Dublication : ");
+            //
+            //foreach (int num  in uniqueArray)
+            //{
+            //    Console.WriteLine(num + " ");
+            //}
+            #endregion
         }
 
         #region Q3 Fun
         //static void ReverseQueue<T>(Queue<T> queue)
         //{
-        //  Stack<T> stack = new Stack<T>();
         //
-        //  while (queue.Count > 0)
-        //  {
-        //      stack.Push(queue.Dequeue());
-        //  }
+        //    Stack<T> stack = new Stack<T>();
+        //
+        //    while (queue.Count > 0)
+        //    {
+        //        stack.Push(queue.Dequeue());
+        //    }
         //}
+
         #endregion
 
         #region Q4 Fun
-        //tatic bool IsBalance(string str)
+        //static bool IsBalance(string str)
+        //{ 
         //
-        //   Stack<char> stack = new Stack<char>();
-        //   Dictionary<char, char> bracketPairs = new Dictionary<char, char>
-        //   {
-        //        {')', '('},
-        //        {']', '['},
-        //        {'}', '{'}
-        //   };
+        //  Stack<char> stack = new Stack<char>();
+        //  Dictionary<char, char> bracketPairs = new Dictionary<char, char>
+        //  {
+        //       {')', '('},
+        //       {']', '['},
+        //       {'}', '{'}
+        //  };
         //
-        //   foreach (char ch in str)
-        //   {
-        //       if (bracketPairs.ContainsValue(ch)) 
-        //       {
-        //           stack.Push(ch);
-        //       }
-        //       else if (bracketPairs.ContainsKey(ch)) 
-        //       {
-        //           if (stack.Count == 0 || stack.Pop() != bracketPairs[ch])
-        //           {
-        //               return false;
-        //           }
-        //       }
-        //   }
+        //  foreach (char ch in str)
+        //  {
+        //      if (bracketPairs.ContainsValue(ch)) 
+        //      {
+        //          stack.Push(ch);
+        //      }
+        //      else if (bracketPairs.ContainsKey(ch)) 
+        //      {
+        //          if (stack.Count == 0 || stack.Pop() != bracketPairs[ch])
+        //          {
+        //              return false;
+        //          }
+        //      }
+        //  }
         //
-        //   return stack.Count == 0;
-            #endregion
-        }
+        //  return stack.Count == 0;
+        //   
+        //}
+        #endregion
+
+        #region Q5 Fun
+        //static int[] RemoveDuplicates(int[] input)
+        //{
+        //    List<int> result = new List<int>();
+        //    foreach (int num in input)
+        //    {
+        //        if (!result.Contains(num))
+        //        {
+        //            result.Add(num);
+        //        }
+        //    }
+        //    return result.ToArray();
+        //}
+        #endregion
     }
 }
